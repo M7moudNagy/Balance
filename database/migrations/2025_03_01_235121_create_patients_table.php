9edbe6ad1f7ee270f65e88389d60427ed88f5154
M7moudNagy <mahmoudnagy4809@gmail.com>
2025-05-05 22:12:38 +0300

    update avatar type

--- database/migrations/2025_03_01_235121_create_patients_table.php
+++ database/migrations/2025_03_01_235121_create_patients_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->enum('gander', ['Male', 'Female'])->default('male');
             $table->string('city');
             $table->string('email')->unique();
-            $table->string('avatar')->default('avatar.png');
+            $table->integer('avatar')->default('0');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
